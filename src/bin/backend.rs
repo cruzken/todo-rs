14158d62ab6da2ae0909e30695333d1890ad999e
Ken Cruz <ken@kencruz.ca>
2019-09-05 15:20:45 -0400

    added db connection pool and sync requests

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -1,28 +1,58 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use todo_rs::db::{models::{JsonApiResponse, TaskJson}, establish_connection, query_task};
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
+use std::ops::Deref;
+use todo_rs::db::{
+    get_connect, init_pool,
+    models::{JsonApiResponse, TaskJson},
+    query_task, SqlitePool,
+};
 
-
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("index\n")
+fn index() -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let result = HttpResponse::Ok().body("test");
+        if result.status().is_success() {
+            Ok("yup")
+        } else {
+            Err("uh oh")
+        }
+    })
+    .then(move |res| match res {
+        Ok(_) => Ok(HttpResponse::Ok().body("test\n")),
+        Err(_) => Ok(HttpResponse::Ok().body("unknown")),
+    })
 }
 
-fn tasks_get() -> impl Responder {
+fn tasks_get(pool: web::Data<SqlitePool>) -> impl Future<Item = HttpResponse, Error = Error> {
+    let pool = pool.clone();
+
     let mut response = JsonApiResponse { data: vec![] };
-    let conn = establish_connection();
-    for task in query_task(&conn) {
-        response.data.push(TaskJson::new(task));
-    }
-    HttpResponse::Ok().json(response)
+
+    web::block(move || query_task(get_connect(&pool).unwrap().deref()))
+        .from_err()
+        .then(move |res| match res {
+            Ok(tasks) => {
+                for task in tasks {
+                    response.data.push(TaskJson::new(task));
+                }
+                return Ok(HttpResponse::Ok().json(response));
+            }
+            Err(e) => Err(e),
+        })
 }
 
 fn main() {
-    HttpServer::new(|| {
+    let pool = init_pool("./testdb.sqlite3").expect("Failed to create pool");
+    let app = move || {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/tasks", web::get().to(tasks_get))
-    })
-    .bind("127.0.0.1:8088")
-    .unwrap()
-    .run()
-    .unwrap();
+            .data(pool.clone())
+            .service(web::resource("/").route(web::get().to_async(index)))
+            .service(web::resource("/tasks").route(web::get().to_async(tasks_get)))
+    };
+
+    println!("Starting server");
+    HttpServer::new(app)
+        .bind("localhost:8088")
+        .unwrap()
+        .run()
+        .unwrap();
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,12 +1,24 @@
+use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
 use diesel::{prelude::*, sqlite::SqliteConnection};
 
 pub mod models;
 pub mod schema;
 
+pub type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
+type SqlitePooledConnection = PooledConnection<ConnectionManager<SqliteConnection>>;
+
+pub fn init_pool(database_url: &str) -> Result<SqlitePool, PoolError> {
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    Pool::builder().build(manager)
+}
+
+pub fn get_connect(pool: &SqlitePool) -> Result<SqlitePooledConnection, &'static str> {
+    pool.get().map_err(|_| "Can't get connection")
+}
+
 pub fn establish_connection() -> SqliteConnection {
     let db = "./testdb.sqlite3";
-    SqliteConnection::establish(db)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", db))
+    SqliteConnection::establish(db).unwrap_or_else(|_| panic!("Error connecting to {}", db))
 }
 
 pub fn create_task<'a>(connection: &SqliteConnection, title: &'a str) {
@@ -19,7 +31,7 @@ pub fn create_task<'a>(connection: &SqliteConnection, title: &'a str) {
 }
 
 pub fn done_update_task(connection: &SqliteConnection, id: i32) {
-    use super::db::schema::task::dsl::{task, done};
+    use super::db::schema::task::dsl::{done, task};
 
     let _ = diesel::update(task.find(id))
         .set(done.eq(1))
@@ -27,19 +39,19 @@ pub fn done_update_task(connection: &SqliteConnection, id: i32) {
         .unwrap_or_else(|_| panic!("Unable to find task {}", id));
 
     let done_task: models::Task = task
-            .find(id)
-            .first(connection)
-            .unwrap_or_else(|_| panic!("Unable to find task {}", id));
+        .find(id)
+        .first(connection)
+        .unwrap_or_else(|_| panic!("Unable to find task {}", id));
     println!("Done task: {}", done_task.title);
 }
 
 pub fn del_task(connection: &SqliteConnection, id: i32) {
-    use super::db::schema::task::dsl::{task};
+    use super::db::schema::task::dsl::task;
 
     let del_task: models::Task = task
-            .find(id)
-            .first(connection)
-            .unwrap_or_else(|_| panic!("Unable to find task {}", id));
+        .find(id)
+        .first(connection)
+        .unwrap_or_else(|_| panic!("Unable to find task {}", id));
 
     let _ = diesel::delete(task.find(id))
         .execute(connection)
@@ -48,8 +60,8 @@ pub fn del_task(connection: &SqliteConnection, id: i32) {
     println!("Deleted task: {}", del_task.title);
 }
 
-pub fn query_task(connection: &SqliteConnection) -> Vec<models::Task> {
-    schema::task::table
-        .load::<models::Task>(&*connection)
-        .expect("Error loading tasks")
+pub fn query_task(
+    connection: &SqliteConnection,
+) -> Result<Vec<models::Task>, diesel::result::Error> {
+    schema::task::table.load::<models::Task>(&*connection)
 }
